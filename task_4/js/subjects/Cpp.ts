066ed4222d3a1977a20c50fc93008fd001390c7b
Christiana-tech <ojofayo56@gmail.com>
2024-12-15 13:56:17 +0100

    commit all

---
+++ task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingC?: number;
+    }
+  
+    export class Cpp extends Subject {
+      teacher: any;
+      getRequirements(): string {
+        return "Here is the list of requirements for Cpp";
+      }
+  
+      getAvailableTeacher(): string {
+        return this.teacher.experienceTeachingC > 0
+          ? `Available Teacher: ${this.teacher.firstName}`
+          : "No available teacher";
+      }
+    }
+  }
+  
